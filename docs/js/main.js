b716d89f90170739d5143f88d65266b834bfb70d
Pascal Reichmuth <pascal.reichmuth@gmail.com>
2017-09-21 23:04:44 +0800

    some updates

--- docs/js/main.js
+++ docs/js/main.js
@@ -19,12 +19,20 @@ function randomName() {
       // generate a random time for the delay
       var delay = Math.floor(Math.random() * 2000) + 1500
       // show the animation for some time
-      $("#trnt").fadeIn(200).delay(delay).slideUp(300);  
+      $("#trnt").fadeIn(200).delay(delay).slideUp(300);
       // extract a noun
         var lines = txt.split("\n");
         var randLineNum = Math.floor(Math.random() * lines.length);
+        // load definition
+        $.ajax({
+          url: "https://api.datamuse.com/words?sp="+lines[randLineNum]+"&md=d"
+        }).done(function(data) {
+          var str=data[0].defs[0].substring(2);
+          $("#description").html(str);
+        });
         // display the noung and show everything again
-        $("#frameworkname").html(lines[randLineNum]+".JS").delay(delay).slideDown(200); // random line from the text file
+        $("#theName").html(lines[randLineNum]+".JS");
+        $("#frameworkname").delay(delay).slideDown(200); // random line from the text file
         $("#generate").delay(delay).slideDown(200);
     });
 }
--- readme.md
+++ readme.md
@@ -1,9 +1,10 @@
-# Javascript Framework Name Generator
+# NAMR - a Name Generator for Javascript Frameworks
 https://digisz.github.io/javascriptframeworknamegenerator/
 Because you really waited for this one to be developed.
 
 ## Contribution
 Yes please!
+This code can't really get worse.
 
 ## Data Source
 http://www.desiquintans.com/downloads/nounlist/nounlist.txt
