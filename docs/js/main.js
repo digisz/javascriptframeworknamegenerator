ba8952a81c34530e1df2fab892e99be35981880e
Kenton Hansen <kenton@roll20.net>
2019-02-18 22:59:40 -0600

    will this even

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,4 +1,5 @@
 $( document ).ready(function() {
+randomAdj();
 randomName();
 
   $( "#generate" ).click(function() {
@@ -11,8 +12,8 @@ randomName();
       });
 });
 
-function randomName() {
-    $.get('txt/adjectives.txt', 'txt/nounlist.txt', function(adjs, txt) {
+function randomAdj() {
+    $.get('txt/adjectives.txt', function(txt) {
       // Hide the latest name and the button
       $("#frameworkname").hide(0);
       $("#generate").hide(0);
@@ -23,8 +24,6 @@ function randomName() {
       // extract a noun
         var lines = txt.split("\n");
         var randLineNum = Math.floor(Math.random() * lines.length);
-        var adjs = txt.split("\n");
-        var randAdjNum = Math.floor(Math.random() * adjs.length);
         // load definition
         $.ajax({
           url: "https://api.datamuse.com/words?sp="+lines[randLineNum]+"&md=d"
@@ -34,7 +33,31 @@ function randomName() {
         });
 
         // display the noun and show everything again
-        $("#theName").html(adjs[randAdjNum] + lines[randLineNum]);
+        $("#theName").html(lines[randLineNum]);
+});
+
+function randomName() {
+    $.get('txt/nounlist.txt', function(txt) {
+      // Hide the latest name and the button
+      $("#frameworkname").hide(0);
+      $("#generate").hide(0);
+      // generate a random time for the delay
+      var delay = Math.floor(Math.random() * 2000) + 1500
+      // show the animation for some time
+      $("#trnt").fadeIn(200).delay(delay).slideUp(300);
+      // extract a noun
+        var lines = txt.split("\n");
+        var randLineNum = Math.floor(Math.random() * lines.length);
+        // load definition
+        $.ajax({
+          url: "https://api.datamuse.com/words?sp="+lines[randLineNum]+"&md=d"
+        }).done(function(data) {
+          var str=data[0].defs[0].substring(2);
+          $("#description").html(str);
+        });
+
+        // display the noun and show everything again
+        $("#theName").html(lines[randLineNum]);
         $("#frameworkname").delay(delay).slideDown(200); // random line from the text file
         $("#generate").delay(delay).slideDown(200);
     });
