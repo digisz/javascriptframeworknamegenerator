b6620d7cfcf44972b6f8cd6d158e0770d9bb9cc2
Kenton Hansen <kenton@roll20.net>
2019-02-18 23:15:07 -0600

    adj definition to definition div

--- docs/js/main.js
+++ docs/js/main.js
@@ -29,7 +29,7 @@ function randomAdj() {
           url: "https://api.datamuse.com/words?sp="+lines[randLineNum]+"&md=d"
         }).done(function(data) {
           var str=data[0].defs[0].substring(2);
-          $("#description").html(str);
+          $("#definition").html(str);
         });
 
         // display the noun and show everything again
@@ -43,9 +43,9 @@ function randomName() {
       $("#frameworkname").hide(0);
       $("#generate").hide(0);
       // generate a random time for the delay
-      var delay = Math.floor(Math.random() * 2000) + 1500
+      // var delay = Math.floor(Math.random() * 2000) + 1500
       // show the animation for some time
-      $("#trnt").fadeIn(200).delay(delay).slideUp(300);
+      // $("#trnt").fadeIn(200).delay(delay).slideUp(300);
       // extract a noun
         var lines = txt.split("\n");
         var randLineNum = Math.floor(Math.random() * lines.length);
