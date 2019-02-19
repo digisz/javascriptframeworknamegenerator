b1f2edb23ed700b4ee4e303531ec04bf8237504b
Kenton Hansen <kenton@roll20.net>
2019-02-18 23:23:12 -0600

    adj definition to definition div

--- docs/js/main.js
+++ docs/js/main.js
@@ -4,6 +4,7 @@ randomName();
 
   $( "#generate" ).click(function() {
     randomName();
+    randomAdj();
     ga('send', {
   hitType: 'event',
   eventCategory: 'RandomName',
@@ -14,26 +15,19 @@ randomName();
 
 function randomAdj() {
     $.get('txt/adjectives.txt', function(txt) {
-      // Hide the latest name and the button
-      // $("#frameworkname").hide(0);
-      // $("#generate").hide(0);
-      // generate a random time for the delay
-      // var delay = Math.floor(Math.random() * 2000) + 1500
-      // show the animation for some time
-      // $("#trnt").fadeIn(200).delay(delay).slideUp(300);
-      // extract a noun
-        var lines = txt.split("\n");
-        var randLineNum = Math.floor(Math.random() * lines.length);
+      // extract a adjective
+        var adj = txt.split("\n");
+        var randAdj = Math.floor(Math.random() * adj.length);
         // load definition
         $.ajax({
-          url: "https://api.datamuse.com/words?sp="+lines[randLineNum]+"&md=d"
+          url: "https://api.datamuse.com/words?sp="+adj[randAdj]+"&md=d"
         }).done(function(data) {
           var str=data[0].defs[0].substring(2);
           $("#definition").html(str);
         });
 
         // display the noun and show everything again
-        $("#theAdj").html(lines[randLineNum]);
+        $("#theAdj").html(adj[randAdj]);
 });
 }
 
