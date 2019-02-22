b8487212fb75e524b5e9e4f2a272ae74213a4cac
Kenton Hansen <kenton@roll20.net>
2019-02-22 09:20:17 -0600

    uses jekyll to serve, picks random definition from array, filters out part of speech text smartly

---
+++ js/main.js
@@ -0,0 +1,63 @@
+$( document ).ready(function() {
+randomAdj();
+randomName();
+
+  $( "#generate" ).click(function() {
+    randomMsg();
+    randomName();
+    randomAdj();
+    ga('send', {
+  hitType: 'event',
+  eventCategory: 'RandomName',
+  eventAction: 'TryAgain'
+});
+      });
+});
+
+function randomAdj() {
+    $.get('txt/adjectives.txt', function(txt) {
+    //$.get('txt/test.txt', function(txt) {
+      // extract a adjective
+        var adj = txt.split("\n");
+        var randAdj = Math.floor(Math.random() * adj.length);
+        // load definition
+        $.ajax({
+          url: "https://api.datamuse.com/words?sp="+adj[randAdj]+"&md=d"
+        }).done(function(data) {
+          var randDef = Math.floor(Math.random() * data[0].defs.length);
+          var str=data[0].defs[randDef].substring(data[0].defs[randDef].indexOf('	')+1);
+          $("#definition").html(str);
+        });
+
+        // display the noun and show everything again
+        $("#theAdj").html(adj[randAdj]);
+});
+}
+
+function randomName() {
+    $.get('txt/nounlist.txt', function(txt) {
+      // Hide the latest name and the button
+      $("#frameworkname").hide(0);
+      $("#generate").hide(0);
+      // generate a random time for the delay
+       var delay = Math.floor(Math.random() * 2000) + 1500
+      // show the animation for some time
+       $("#trnt").fadeIn(200).delay(delay).slideUp(300);
+      // extract a noun
+        var lines = txt.split("\n");
+        var randLineNum = Math.floor(Math.random() * lines.length);
+        // load definition
+        $.ajax({
+          url: "https://api.datamuse.com/words?sp="+lines[randLineNum]+"&md=d"
+        }).done(function(data) {
+          var randDef = Math.floor(Math.random() * data[0].defs.length);
+          var str=data[0].defs[randDef].substring(data[0].defs[randDef].indexOf('	')+1);
+          $("#description").html(str);
+        });
+
+        // display the noun and show everything again
+        $("#theNoun").html(lines[randLineNum]);
+        $("#frameworkname").delay(delay).slideDown(200); // random line from the text file
+        $("#generate").delay(delay).slideDown(200);
+    });
+}
