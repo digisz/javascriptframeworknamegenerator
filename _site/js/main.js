7e92d7dd3a709fb42768257151c124718faaf074
Kenton Hansen <kenton@roll20.net>
2020-08-08 00:58:13 -0500

    engages yolo protocol - words removed from list once used. Adds recall via cookie. fixes logo on small screens.

--- _site/js/main.js
+++ _site/js/main.js
@@ -4,7 +4,7 @@ var buttons = [];
 var holdon = [];
 
 $( document ).ready(function() {
-  $("#theNoun").hide(0);
+  $("#theName").hide(0);
   $("#frameworkname").hide(0);
   $("#makeName").hide(0);
   $("#hold").text("loading...")
@@ -26,43 +26,54 @@ $( document ).ready(function() {
     })
   ).then(function () {
     randomAdj();
-    randomName();
+    randomNoun();
+    go();
     randomMsg();
+    yolo();
     ga('send', {
       hitType: 'event',
-      eventCategory: 'RandomName',
+      eventCategory: 'go',
       eventAction: 'FirstTime'
     });
   });
 
   $( "#makeName" ).click(function() {
+    setLast();
     randomMsg();
-    randomName();
+    randomNoun();
+    go();
+    yolo();
     randomAdj();
     ga('send', {
       hitType: 'event',
-      eventCategory: 'RandomName',
+      eventCategory: 'go',
       eventAction: 'TryAgain'
     });
   });
   
   $( "#recall" ).click(function() {
-    var prevNoun = getCookie('last_noun');
-    $("#definition").text(prevNoun);
+    go();
+    var recallLast = getCookie("last").split("!");
+    var recallAdj = recallLast[0];
+    var recallNoun = recallLast[1];
+    define(recallAdj, recallNoun);
+    $("#theNoun").text(recallNoun);
+    $("#theAdj").text(recallAdj);
   });
 });
 
 function getCookie(name) {
-  var nameEQ = name + "=";
-  var ca = document.cookie.split(';');
-  for(var i=0;i < ca.length;i++) {
-      var c = ca[i];
-      while (c.charAt(0)==' ') c = c.substring(1,c.length);
-      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-  }
-  return null;
+  var regexp = new RegExp("(?:^" + name + "|;\s*"+ name + ")=(.*?)(?:;|$)", "g");
+  var result = regexp.exec(document.cookie);
+  return (result === null) ? null : result[1];
 }
 
+function setLast() {
+  // Saves current words to cookie for potential recall
+  var CookieDate = new Date;
+    CookieDate.setFullYear(CookieDate.getFullYear() + 1);
+  document.cookie = "last="+currentAdj+"!"+currentNoun+"; expires=" + CookieDate.toUTCString() + ";";
+};
 
 function randomMsg() {
   // Insert text into button
@@ -73,38 +84,55 @@ function randomMsg() {
   $("#hold").text(holdon[randHold]);
 };
 
+function yolo() {
+  nounlist = nounlist.filter(e => e !== currentNoun);
+  console.log(nounlist.length);
+  adjectives = adjectives.filter(e => e !== currentAdj);
+  console.log(adjectives.length);
+}
+
+//should be able to DRY definitions up
+function define(adj, noun) {
+  $.ajax({
+    url: "https://api.datamuse.com/words?sp="+adj+"&md=d"
+  }).done(function(data) {
+    var randDef = Math.floor(Math.random() * data[0].defs.length);
+    var str=data[0].defs[randDef].substring(data[0].defs[randDef].replace('\t', ' ').indexOf(' ')+1);
+    $("#definition").text(str.charAt(0).toUpperCase() + str.slice(1));
+  });
+  $.ajax({
+    url: "https://api.datamuse.com/words?sp="+noun+"&md=d"
+  }).done(function(data) {
+    var randDef = Math.floor(Math.random() * data[0].defs.length);
+    var str=data[0].defs[randDef].substring(data[0].defs[randDef].replace('\t', ' ').indexOf(' ')+1);
+    $("#description").text(str.charAt(0).toUpperCase() + str.slice(1));
+  });
+}
+
 function randomAdj() {
   // extract a adjective
   var randAdj = Math.floor(Math.random() * adjectives.length);
-  document.cookie = "last_adj="+adjectives[randAdj]+"; expires=Sun, 1 Jan 2023 00:00:00 UTC; path=/";
+  currentAdj = adjectives[randAdj];
   // load definition
   $.ajax({
-    url: "https://api.datamuse.com/words?sp="+adjectives[randAdj]+"&md=d"
+    url: "https://api.datamuse.com/words?sp="+currentAdj+"&md=d"
   }).done(function(data) {
     var randDef = Math.floor(Math.random() * data[0].defs.length);
     var str=data[0].defs[randDef].substring(data[0].defs[randDef].replace('\t', ' ').indexOf(' ')+1);
     $("#definition").text(str.charAt(0).toUpperCase() + str.slice(1));
   });
 
-  // display the noun and show everything again
-  $("#theAdj").text(adjectives[randAdj]);
+  // display the adj and show everything again
+  $("#theAdj").text(currentAdj);
 }
 
-function randomName() {
-  // Hide the latest name and the button
-  $("#theNoun").hide(0);
-  $("#frameworkname").hide(0);
-  $("#makeName").hide(0);
-  // generate a random time for the delay
-  var delay = Math.floor(Math.random() * 1000) + 1500
-  // show the animation for some time
-  $("#trnt").fadeIn(200).delay(delay).slideUp(300);
+function randomNoun() {
   // extract a noun
   var randLineNum = Math.floor(Math.random() * nounlist.length);
-  document.cookie = "last_noun="+nounlist[randLineNum]+"; expires=Sun, 1 Jan 2023 00:00:00 UTC; path=/";
+  currentNoun = nounlist[randLineNum];
   // load definition
   $.ajax({
-    url: "https://api.datamuse.com/words?sp="+nounlist[randLineNum]+"&md=d"
+    url: "https://api.datamuse.com/words?sp="+currentNoun+"&md=d"
   }).done(function(data) {
     if (typeof data[0].defs === 'undefined') {
       $("#description").hide();
@@ -113,11 +141,24 @@ function randomName() {
       var str=data[0].defs[randDef].substring(data[0].defs[randDef].replace('\t', ' ').indexOf(' ')+1);
       // display the noun and show everything again
       $("#description").text(str.charAt(0).toUpperCase() + str.slice(1)).show();
-    }
-  }).always(function() {
-    $("#theNoun").text(nounlist[randLineNum]).slideDown(200);
-    $("#frameworkname").delay(delay).slideDown(200); // random line from the text file
-    $("#makeName").delay(delay).slideDown(200);
+    };
   })
+  $("#theNoun").text(currentNoun);
+};
 
-}
+function go() {
+  // Hide the latest name and the button
+  $("#theName").hide(0);
+  $("#frameworkname").hide(0);
+  $("#makeName").hide(0);
+  $("#caret").hide(0);
+  // generate a random time for the delay
+  var delay = Math.floor(Math.random() * 1000) + 1500
+  // show the animation for some time
+  $("#trnt").fadeIn(200).delay(delay - 200).hide(0);
+  $("#frameworkname").delay(delay).slideDown(200); // random line from the text file
+  $("#makeName").delay(delay).slideDown(200);
+  $("#caret").delay(delay).slideDown(200);
+  $("#theName").delay(delay).slideDown(200);
+  $("#theMeaning").delay(delay).slideDown(200);
+};
